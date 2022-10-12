ec20f6f8bc215759efb4fd7dd595364b298542a7
Leda <ledathemis@protonmail.com>
2022-10-12 20:33:41 +0000

    feat: add pages skeleton

---
+++ src/pages/app/progress.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Sidebar from "../../components/App/SidebarNav";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Progress - Measura</title>
+        <meta
+          name="description"
+          content="View your progress as a nicely-drawn graph in Measura."
+        />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <main className="flex">
+        <Sidebar />
+        <section className="mt-10 flex flex-grow justify-center">
+          <h2 className="text-4xl font-bold text-gray-600">Coming soon</h2>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/pages/app/settings.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Sidebar from "../../components/App/SidebarNav";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Settings - Measura</title>
+        <meta
+          name="description"
+          content="Configure preferences and select your preferred unit system in Measura."
+        />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <main className="flex">
+        <Sidebar />
+        <section className="mt-10 flex flex-grow justify-center">
+          <h2 className="text-4xl font-bold text-gray-600">Coming soon</h2>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default Home;
