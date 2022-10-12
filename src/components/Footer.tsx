6e8f971fad288975cc8f974f36d465e293f64b78
Leda <ledathemis@protonmail.com>
2022-10-12 12:32:51 +0000

    feat: add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,40 @@
+import { BsGithub, BsGlobe2 } from "react-icons/bs";
+import RulerIcon from "../icons/RulerIcon";
+
+const Footer = () => {
+  return (
+    <footer className="body-font text-gray-600">
+      <div className="container mx-auto flex flex-col items-center px-5 py-8 sm:flex-row">
+        <a className="title-font flex items-center justify-center font-medium text-gray-900 md:justify-start">
+          <RulerIcon />
+          <span className="ml-3 text-xl">Measura</span>
+        </a>
+        <p className="mt-4 text-sm text-gray-500 sm:ml-4 sm:mt-0 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:pl-4">
+          © 2022 Leda
+        </p>
+        <span className="mt-4 inline-flex justify-center sm:ml-auto sm:mt-0 sm:justify-start">
+          <a
+            href="https://github.com/LedaThemis/measura"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="ml-3 text-gray-500 transition-colors duration-150 hover:text-gray-800"
+          >
+            <BsGithub aria-hidden="true" className="h-5 w-5 cursor-pointer" />
+            <span className="sr-only">Github (opens in new tab)</span>
+          </a>
+          <a
+            href="https://www.leda.dev"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="ml-3 text-gray-500 transition-colors duration-150 hover:text-gray-800"
+          >
+            <BsGlobe2 aria-hidden="true" className="h-5 w-5 cursor-pointer" />
+            <span className="sr-only">Personal website (opens in new tab)</span>
+          </a>
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,12 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { signIn, signOut, useSession } from "next-auth/react";
-import { trpc } from "../utils/trpc";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Home/Hero";
 import Features from "../components/Home/Features";
 import Contact from "../components/Home/Contact";
+import Footer from "../components/Footer";
 
 const Home: NextPage = () => {
-  const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Head>
@@ -25,38 +22,10 @@ const Home: NextPage = () => {
         <Hero />
         <Features />
         <Contact />
-        <div className="flex w-full items-center justify-center pt-6 text-2xl text-blue-500">
-          {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
-        </div>
-        <AuthShowcase />
+        <Footer />
       </main>
     </>
   );
 };
 
 export default Home;
-
-const AuthShowcase: React.FC = () => {
-  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery();
-
-  const { data: sessionData } = useSession();
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-2">
-      {sessionData && (
-        <p className="text-2xl text-blue-500">
-          Logged in as {sessionData?.user?.id}
-        </p>
-      )}
-      {secretMessage && (
-        <p className="text-2xl text-blue-500">{secretMessage}</p>
-      )}
-      <button
-        className="rounded-md border border-black bg-violet-50 px-4 py-2 text-xl shadow-lg hover:bg-violet-100"
-        onClick={sessionData ? () => signOut() : () => signIn("email")}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-};
