d79050a7c4fa240c9e624ad066c4c47bccb4ff8b
Leda <ledathemis@protonmail.com>
2022-10-30 11:17:58 +0000

    feat: make new entry button full width

--- src/components/pages/App/NewEntryButton.tsx
+++ src/components/pages/App/NewEntryButton.tsx
@@ -5,9 +5,9 @@ const NewEntryButton = () => {
   const dialogRef = useRef<HTMLDialogElement>(null);
 
   return (
-    <section className="mt-10 flex flex-grow justify-center">
+    <section className="mx-4 my-10 flex flex-grow">
       <button
-        className="w-full max-w-lg rounded-md bg-blue-500 px-4 py-2 text-lg text-white hover:bg-blue-600"
+        className="w-full rounded-md bg-blue-500 px-4 py-2 text-lg text-white hover:bg-blue-600"
         onClick={() => {
           dialogRef.current?.showModal();
         }}
