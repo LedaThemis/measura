5a43c52e25e706f9646fb2e78c587a1aaa0fd3c6
Leda <ledathemis@protonmail.com>
2022-10-31 02:02:47 +0000

    fix: progress page client-side error

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,8 @@ function defineNextConfig(config) {
 
 export default defineNextConfig({
   reactStrictMode: true,
-  swcMinify: true,
+  // https://github.com/chartjs/Chart.js/issues/10673
+  swcMinify: false,
   // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
   i18n: {
     locales: ["en"],
